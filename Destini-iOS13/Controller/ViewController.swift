291a4ee41d8334aec716443d48b5555293174555
Sasha Evtushenko <sasha.evtushenko.md@gmail.com>
2020-07-28 23:28:17 +0300

    Storytelling app is ready with MVC Design Pattern.

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateUI(with: storyBrain.currentStory)
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        updateUI(with: storyBrain.currentStory)
+    }
+    
+    func updateUI(with storyIndex: Int) {
+        storyLabel.text = storyBrain.stories[storyIndex].title
+        choice1Button.setTitle(storyBrain.stories[storyIndex].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.stories[storyIndex].choice2, for: .normal)
+    }
+    
 }
 
